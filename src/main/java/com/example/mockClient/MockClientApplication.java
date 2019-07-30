e159b0e3860aeeaffb061c40ed1ff0219ee8731b
margaritaq <margaritaq@mail.ru>
2019-07-30 14:39:23 +0300

    path chenge commit

--- src/main/java/com/example/mockClient/MockClientApplication.java
+++ src/main/java/com/example/mockClient/MockClientApplication.java
@@ -4,12 +4,14 @@ package com.example.mockClient;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.servlet.ServletException;
@@ -18,6 +20,8 @@ import java.io.File;
 @SpringBootApplication
 @EnableScheduling
 public class MockClientApplication {
+	@Autowired
+	private Environment environment;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MockClientApplication.class, args);
@@ -34,7 +38,8 @@ public class MockClientApplication {
 				new File(tomcat.getServer().getCatalinaBase(), "webapps").mkdirs();
 
 				try {
-					Context context = tomcat.addWebapp("/app", "C:\\My Program Files\\myApp\\wars\\Mock.war");
+					String warPath= environment.getProperty("war.path");
+					Context context = tomcat.addWebapp("/app", warPath);
 					context.setParentClassLoader(getClass().getClassLoader());
 				} catch (ServletException ex) {
 					throw new IllegalStateException("Failed to add webapp", ex);
